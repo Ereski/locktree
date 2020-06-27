545e801da33bf1f4c5f73206e188af564b2a5015
Carol Schulze <carol@ereski.org>
2020-06-27 12:54:54 -0300

    Run tokio tests by default

--- locktree/tests/compiletest.rs
+++ locktree/tests/compiletest.rs
@@ -3,7 +3,5 @@ fn compiletest() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/compilefail/*.rs");
     t.pass("tests/compilepass/*.rs");
-
-    #[cfg(feature = "tokio")]
     t.pass("tests/compilepass-tokio/*.rs");
 }
